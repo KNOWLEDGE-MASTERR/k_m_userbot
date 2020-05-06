071af39d3fc9d787b9c4997541d9494d1ed9da90
knowledge-master <64637863+knowledge-master@users.noreply.github.com>
2020-05-06 12:08:55 +0530

    Update and rename pureindialover.py to knowledge_masterr.py

--- userbot/plugins/knowledge_masterr.py
+++ userbot/plugins/knowledge_masterr.py
@@ -1,7 +1,7 @@
 """Emoji
 Available Commands:
 .pureindialover
-Credits to @pureindialover
+Credits to @knowledge_masterr
 """
 
 from telethon import events
@@ -11,23 +11,23 @@ import asyncio
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("pureindialover"))
+@borg.on(admin_cmd("knowledge_masterr"))
 async def _(event):
     if event.fwd_from:
         return
     animation_interval = 1.5
     animation_ttl = range(0,36)
     #input_str = event.pattern_match.group(1)
-   # if input_str == "pureindialover":
-    await event.edit("@pureindialover")
+   # if input_str == "knowledge_masterr":
+    await event.edit("@knowledge_masterr")
     animation_chars = [
-            "@pureindialover tera baap",
-            "@pureindialover is bot ka creator",
-            "@pureindialover bot ko jaan dene wala",
-            "@pureindialover owner of @IndianArMyGiveaway ",
+            "@knowledge_masterr tera baap",
+            "@knowledge_masterr is bot ka creator",
+            "@knowledge_masterr bot ko jaan dene wala",
+            "@knowledge_masterr owner of TELEGRAM ",
             "tujhe aur kya chaiye vo hai mere sath",
             "tera baap",
-            "@pureindialover"
+            "@knowledge_masterr"
          ]
             
 
