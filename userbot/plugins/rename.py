e968215322281b18ee8bc1b665f6264a054ba9a6
knowledge-master <64637863+knowledge-master@users.noreply.github.com>
2020-05-06 12:14:02 +0530

    Update rename.py

--- userbot/plugins/rename.py
+++ userbot/plugins/rename.py
@@ -167,7 +167,7 @@ async def _(event):
                     event.chat_id,
                     downloaded_file_name,
                     thumb=thumb,
-                    caption="reuploaded by [IndianBot](https://www.github.com/indianbhaiya/IndianBot",
+                    caption="reuploaded by [Knowledge-Master](t.me/knowledge_masterr)",
                     force_document=False,
                     allow_cache=False,
                     reply_to=event.message.id,
